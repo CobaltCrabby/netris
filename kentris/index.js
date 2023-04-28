58225b2a3d780417ceea6003b2633579c58eecdb
Jack Davidson <jackmaverickavidson@gmail.com>
2023-04-28 10:27:13 -0500

    added empty routes

--- kentris/index.js
+++ kentris/index.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
     res.send('KENTRIS');
 });
 
+/* access session by id. */
+app.get('/session/:id', (req, res) => {
+});
+
+/* update session by id. */
+app.put('/session/:id', (req, res) => {
+});
+
+/* create a new session, returns id. */
+app.post('/newsession', (req, res) => {
+});
+
+/* End session. */
+app.delete('/endsession/:id', (req, res) => {
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
